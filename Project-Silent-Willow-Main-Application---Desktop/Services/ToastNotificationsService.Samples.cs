2b797fc7beba1127b40a8047958d828c76db4c59
324Hz <1024Bitcoins@gmail.com>
2022-10-03 21:49:41 -0500

    commit

--- Project-Silent-Willow-Main-Application---Desktop/Services/ToastNotificationsService.Samples.cs
+++ Project-Silent-Willow-Main-Application---Desktop/Services/ToastNotificationsService.Samples.cs
@@ -1,45 +1,32 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
-
 using Windows.UI.Notifications;
 
-namespace Project_Silent_Willow_Main_Application___Desktop.Services
-{
-    internal partial class ToastNotificationsService
-    {
-        public void ShowToastNotificationSample()
-        {
+namespace Project_Silent_Willow_Main_Application___Desktop.Services {
+    internal partial class ToastNotificationsService {
+        public void ShowToastNotificationSample() {
             // Create the toast content
-            var content = new ToastContent()
-            {
+            var content = new ToastContent() {
                 // More about the Launch property at https://docs.microsoft.com/dotnet/api/microsoft.toolkit.uwp.notifications.toastcontent
                 Launch = "ToastContentActivationParams",
 
-                Visual = new ToastVisual()
-                {
-                    BindingGeneric = new ToastBindingGeneric()
-                    {
-                        Children =
-                        {
-                            new AdaptiveText()
-                            {
+                Visual = new ToastVisual() {
+                    BindingGeneric = new ToastBindingGeneric() {
+                        Children = {
+                            new AdaptiveText() {
                                 Text = "Sample Toast Notification"
                             },
 
-                            new AdaptiveText()
-                            {
+                            new AdaptiveText() {
                                  Text = @"Click OK to see how activation from a toast notification can be handled in the ToastNotificationService."
                             }
                         }
                     }
                 },
 
-                Actions = new ToastActionsCustom()
-                {
-                    Buttons =
-                    {
+                Actions = new ToastActionsCustom() {
+                    Buttons = {
                         // More about Toast Buttons at https://docs.microsoft.com/dotnet/api/microsoft.toolkit.uwp.notifications.toastbutton
-                        new ToastButton("OK", "ToastButtonActivationArguments")
-                        {
+                        new ToastButton("OK", "ToastButtonActivationArguments") {
                             ActivationType = ToastActivationType.Foreground
                         },
 
@@ -49,8 +36,7 @@ namespace Project_Silent_Willow_Main_Application___Desktop.Services
             };
 
             // Add the content to the toast
-            var toast = new ToastNotification(content.GetXml())
-            {
+            var toast = new ToastNotification(content.GetXml()) {
                 // TODO: Set a unique identifier for this notification within the notification group. (optional)
                 // More details at https://docs.microsoft.com/uwp/api/windows.ui.notifications.toastnotification.tag
                 Tag = "ToastTag"
