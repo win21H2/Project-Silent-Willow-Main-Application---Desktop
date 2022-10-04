b7cf7334d5794ca925ed58bc9f3d1ee46b6f5b4d
324Hz <1024Bitcoins@gmail.com>
2022-10-03 23:29:31 -0500

    commit

--- Project-Silent-Willow-Main-Application---Desktop/Views/MainPage.xaml.cs
+++ Project-Silent-Willow-Main-Application---Desktop/Views/MainPage.xaml.cs
@@ -1,18 +1,19 @@
-﻿using System;
-
-using Project_Silent_Willow_Main_Application___Desktop.ViewModels;
-
+﻿using Project_Silent_Willow_Main_Application___Desktop.ViewModels;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
-namespace Project_Silent_Willow_Main_Application___Desktop.Views
-{
-    public sealed partial class MainPage : Page
-    {
+namespace Project_Silent_Willow_Main_Application___Desktop.Views {
+    public sealed partial class MainPage : Page {
         public MainViewModel ViewModel { get; } = new MainViewModel();
 
-        public MainPage()
-        {
+        public MainPage() {
             InitializeComponent();
+        }
+
+        void BTON(object sender, RoutedEventArgs e) {
+            Button button = sender as Button;
+            button.Background = new SolidColorBrush(Windows.UI.Colors.Red);
         }
     }
 }
