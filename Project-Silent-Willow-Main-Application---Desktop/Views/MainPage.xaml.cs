5577d77e548350c8e6a50f602f78dfd575b57e2f
324Hz <1024Bitcoins@gmail.com>
2022-10-07 08:17:48 -0500

    commit

--- Project-Silent-Willow-Main-Application---Desktop/Views/MainPage.xaml.cs
+++ Project-Silent-Willow-Main-Application---Desktop/Views/MainPage.xaml.cs
@@ -11,9 +11,29 @@ namespace Project_Silent_Willow_Main_Application___Desktop.Views {
             InitializeComponent();
         }
         
-        void CONNECT_Click(object sender, RoutedEventArgs e) {
-            CONNECT.Foreground = new SolidColorBrush(Windows.UI.Colors.Green);
-            DISCONNECT.Foreground = new SolidColorBrush(Windows.UI.Colors.Blue);
+        void CONNECT_Click(object sender, RoutedEventArgs e) {  
+            TESTBUTTON.Foreground = new SolidColorBrush(Windows.UI.Colors.Green);
+            // MAKE IT LAUNCH A TOAST IF THE USER DOES NOT HAVE BLUETOOTH TURNED ON
+        }
+
+        void DISCONNECT_Click(object sender, RoutedEventArgs e) {
+            TESTBUTTON.Foreground = new SolidColorBrush(Windows.UI.Colors.Blue);
+            // MAKE IT LAUNCH A TOAST WHICH DISPLAYS THAT THE COMPUTER HAS DISCONNECTED
+        }
+
+        void BTON_Click(object sender, RoutedEventArgs e) {
+            TESTBUTTON.Foreground = new SolidColorBrush(Windows.UI.Colors.Yellow);
+            // TURN BLUETOOTH ON
+        }
+        
+        void BTOFF_Click(object sender, RoutedEventArgs e) {
+            TESTBUTTON.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+            // TURN BLUETOOTH OFF
+        }
+
+        void TESTBUTTON_Click(object sender, RoutedEventArgs e) {
+            TESTBUTTON.Foreground = new SolidColorBrush(Windows.UI.Colors.White);
+            // NO EXTRA FUNCTIONS NEEDED
         }
     }
 }
